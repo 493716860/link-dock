4c12d5ad4a7f18bf10d44c6a822f5b6b115bb94b
this.yoyo <493716860@163.com>
2026-05-08 11:18:27 +0800

    更新了扩展程序里面默认环境

--- extension/config.js
+++ extension/config.js
@@ -5,6 +5,6 @@
  * 例如：'https://your-domain.com/'
  */
 window.LinkDockConfig = {
-  baseUrl: 'http://localhost:3000/',// 开发环境
-  // baseUrl:'https://www.superyoyo.ren/',// 生产环境
+  // baseUrl: 'http://localhost:3000/',// 开发环境
+  baseUrl:'https://www.superyoyo.ren/',// 生产环境
 };
